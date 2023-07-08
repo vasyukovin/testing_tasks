3c29ae359809ebc0b4236218837cdb76ec7daa46
Igor Vasyukov <vasyukovin@yandex.ru>
2023-07-08 17:50:24 +0300

    solution

--- solid_d.php
+++ solid_d.php
@@ -5,7 +5,7 @@ class XMLHttpService extends XMLHTTPRequestService {}
 class Http {
     private $service;
 
-    public function __construct(XMLHttpService $xmlHttpService) { }
+    public function __construct(XMLHTTPRequestService $xmlHttpService) { }
 
     public function get(string $url, array $options) {
         $this->service->request($url, 'GET', $options);
